3d670a231eea3d0c9ee1f5ad7f93e2e096de6976
AdliAz <48936469+AdliAz@users.noreply.github.com>
2020-10-20 16:40:35 +1030

    Restart

---
+++ Adelaide Fringe Application/Adelaide Fringe Application/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Adelaide Fringe Application
+//
+//  Created by A Azhar, Adlisyam - ayaay002 on 20/10/20.
+//  Copyright © 2020 A Azhar, Adlisyam - ayaay002. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
